7e87b1773b632818934fa4f125273545e0efd837
Ching-lin LEE <chilee@student.42.fr>
2022-07-04 20:29:32 +0200

    update0040722_2

--- p3/scripts/create_cluster_run_service.sh
+++ p3/scripts/create_cluster_run_service.sh
@@ -5,41 +5,38 @@
 # -p, --port [HOST:][HOSTPORT:]CONTAINERPORT[/PROTOCOL][@NODEFILTER]
  
 echo "creating cluster..."
-k3d cluster create Part3 -p 8080:80@loadbalancer -p 8888:30888@loadbalancer
+sudo k3d cluster create Part3 -p 8080:80@loadbalancer -p 8888:30888@loadbalancer
 sleep 5
 
 echo "creating namespaces..."
-kubectl create namespace argocd
-kubectl create namespace dev
+sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
 sleep 3
 
 echo "installing argocd"
-kubectl apply -f ../confs/install.yaml -n argocd
+sudo kubectl apply -f ../confs/install.yaml -n argocd
 sleep 3
 
 echo "waiting the pods to be ready"
-kubectl wait -n argocd --for=condition=Ready pods --all
+sudo kubectl wait -n argocd --for=condition=Ready pods --all
 sleep 3
 
 echo "deploy ingress of argocd..."
-kubectl apply -f ../confs/ingress.yaml -n argocd
+sudo kubectl apply -f ../confs/ingress.yaml -n argocd
 sleep 3
 
 
 echo "changing defautl password to "password""
 #bcrypt(password)=$2a$10$rRyBsGSHK6.uc8fntPwVIuLVHgsAhAX7TcdrqW/RADU0uh7CaChLa
-kubectl -n argocd patch secret argocd-secret \
+sudo kubectl -n argocd patch secret argocd-secret \
   -p '{"stringData": {
     "admin.password": "$2a$10$rRyBsGSHK6.uc8fntPwVIuLVHgsAhAX7TcdrqW/RADU0uh7CaChLa",
     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
-  }}
+  }}'
 sleep 3
 
 echo "installing application to argocd"
-kubectl apply -f ../confs/project.yaml -n argocd
+sudo kubectl apply -f ../confs/project.yaml -n argocd
 sleep 3
 
 echo "Done" 
-
-
-
