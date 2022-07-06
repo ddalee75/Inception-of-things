3cf24cbe582ecb080c75bd78537c266e062b3b53
Ching-lin LEE <chilee@student.42.fr>
2022-07-06 15:55:03 +0200

    update060722V3

--- p3/scripts/install_environnement.sh
+++ p3/scripts/install_environnement.sh
@@ -19,8 +19,11 @@ sudo sh ./get-docker.sh
 echo "installing kubectl..."
 #download kubectl
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
 #install kubectl
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubecitl
 
 echo "installing k3d..."
 sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
