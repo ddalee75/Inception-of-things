6f7fefd8afcf4e78b9db6ea53e631cb4093b6957
Ching-lin LEE <chilee@student.42.fr>
2022-07-07 14:40:49 +0200

    p3test0707

---
+++ p3test/scripts/clean.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# uninstall curl, docker, kubectl, k3d...
+
+#uninstall docker
+echo "\nuninstalling docker..."
+sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
+sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce 
+echo "removing all docker's files... "
+sudo rm -rf /var/lib/docker /etc/docker
+sudo rm /etc/apparmor.d/docker
+sudo groupdel docker
+sudo rm -rf /var/run/docker.sock
+echo "docker unstalled"
+
+# uninstall curl
+echo "\nuninstalling curl"
+sudo apt-get remove curl -y
+echo "curl unstalled"
+
+# uninstall k3d and kubectl
+echo "\nuninstalling k3d and kubectl..."
+sudo rm -rf /usr/local/bin/k3d
+sudo rm -rf /usr/local/bin/kubectl
+echo "done"
---
+++ p3test/scripts/create_cluster_run_service.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# create cluster and run service 
+
+# Create a cluster, mapping the ingress port 80(container) to localhost:8080
+# -p, --port [HOST:][HOSTPORT:]CONTAINERPORT[/PROTOCOL][@NODEFILTER]
+# et 8888 to 30888 nodeport of application
+ 
+echo "creating cluster..."
+sudo k3d cluster create -p 8080:80@loadbalancer -p 8888:30888@loadbalancer
+sleep 5
+
+echo "creating namespaces..."
+sudo kubectl create namespace argocd
+sudo kubectl create namespace dev
+sleep 3
+
+echo "installing argocd"
+sudo kubectl apply -f -n argocd ../confs/install.yaml
+sleep 3
+
+
+#echo "waiting the pods to be ready"
+#sudo kubectl wait -n argocd --for=condition=Ready pods --all
+#sleep 3
+
+echo "deploy ingress of argocd..."
+sudo kubectl apply -f ../confs/ingress.yaml -n argocd
+sleep 3
+
+
+echo "changing defautl password"
+#bcrypt(password)=$2a$10$rRyBsGSHK6.uc8fntPwVIuLVHgsAhAX7TcdrqW/RADU0uh7CaChLa
+sudo kubectl -n argocd patch secret argocd-secret \
+  -p '{"stringData": {
+    "admin.password": "$2a$10$rRyBsGSHK6.uc8fntPwVIuLVHgsAhAX7TcdrqW/RADU0uh7CaChLa",
+    "admin.passwordMtime": "'$(date +%FT%T%Z)'"
+  }}'
+sleep 3
+
+echo "installing project to argocd"
+sudo kubectl apply -f ../confs/project.yaml -n argocd
+echo "installed project to argocd"
+sleep 3
+
+echo "installing application to argocd"
+sudo kubectl apply -f ../confs/application.yaml -n argocd
+sleep 3
+
+echo "Done" 
