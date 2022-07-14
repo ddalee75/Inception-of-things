5cd701194773dfc6755270e45922fd6da0898f58
Ching-lin LEE <chilee@student.42.fr>
2022-07-14 14:30:39 +0200

    update01407v1

--- bonus_V2/scripts/bonus.sh
+++ bonus_V2/scripts/bonus.sh
@@ -5,15 +5,15 @@ HOST_IP=$(hostname -I | awk '{print $2}')
 sudo k3d cluster edit adconsta --port-add 8889:30889@loadbalancer
 
 sudo kubectl create namespace gitlab
-echo "\n----------------Bonus-----------------"
-echo "\ninstalling project Bonus to argocd..."
+echo -e "\n----------------Bonus-----------------"
+echo -e "\ninstalling project Bonus to argocd..."
 
 if grep -q HOST_IP ../confs/project.yaml; then
     sed -i 's/HOST_IP/'"$HOST_IP"'/g' ../confs/project.yaml
 fi
 
 sudo kubectl apply -f ../confs/project.yaml -n argocd
-echo "\ninsatlled progect to argocd"
+echo -e "\ninsatlled progect to argocd"
 sleep 3
 
 echo "\ninstalling application Bonus to argocd..."
@@ -22,7 +22,7 @@ if grep -q HOST_IP ../confs/application.yaml; then
 fi
 
 sudo kubectl apply -f ../confs/application.yaml -n argocd
-echo "\ninsatlled application to argocd"
+echo -e "\ninsatlled application to argocd"
 sleep 3
 
 echo "Done for Bonus !"
